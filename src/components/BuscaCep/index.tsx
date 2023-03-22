cf5a7e2a74722d55c6e8f094703682d53f18101f
Márcio Vinícius <marcio.campos@outlook.com>
2023-03-22 15:28:29 -0300

    feat: create viacep page

---
+++ src/components/BuscaCep/index.tsx
@@ -0,0 +1,15 @@
+import Container from 'components/Container'
+
+import * as S from './styles'
+
+function BuscaCep() {
+  return (
+    <Container>
+      <S.BuscaCepContainer>
+        <S.Title>ViaCep</S.Title>
+      </S.BuscaCepContainer>
+    </Container>
+  )
+}
+
+export default BuscaCep
---
+++ src/pages/viacep.tsx
@@ -0,0 +1,13 @@
+import Header from 'components/Header'
+import BuscaCep from 'components/BuscaCep'
+import Footer from 'components/Footer'
+
+export default function Table() {
+  return (
+    <>
+      <Header />
+      <BuscaCep />
+      <Footer />
+    </>
+  )
+}
