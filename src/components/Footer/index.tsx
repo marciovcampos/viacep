6bb73aaca0ff9df19a94ca2886f86ac46b8668ab
Márcio Vinícius <marcio.campos@outlook.com>
2023-03-22 14:57:15 -0300

    feat: create footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,11 @@
 import Container from 'components/Container'
 import * as S from './styles'
 
-import LogoCoinSynch from 'assets/coin-synch.svg'
-
 function Footer() {
   return (
     <S.Wrapper>
       <Container>
-        <S.FooterContainer>
-          <S.Text> Copyright @ 2023 - All rights reserved</S.Text>
-
-          <S.Logo src={LogoCoinSynch.src} alt="CoinSynch Logo"></S.Logo>
-        </S.FooterContainer>
+        <S.Text> Copyright @ 2023 - All rights reserved</S.Text>
       </Container>
     </S.Wrapper>
   )
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -7,23 +7,12 @@ export const Wrapper = styled.div`
   background-color: ${theme.colors.white};
   box-shadow: 0px -4px 8px rgba(77, 77, 77, 0.1);
   display: flex;
-  justify-content: space-between;
   padding-top: 1.5rem;
   padding-bottom: 1.5rem;
 `
 
-export const FooterContainer = styled.div`
-  display: flex;
-`
-
 export const Text = styled.text`
-  flex: 1;
+  align-content: center;
   font-size: ${theme.font.sizes.regular.label};
   color: ${theme.colors.textBase};
-`
-
-export const Logo = styled.img`
-  width: 5.875rem;
-  height: 1rem;
-  float: right;
 `
