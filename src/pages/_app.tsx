db051751d40aff977d7f279b74ade7db13100f91
Márcio Vinícius <marcio.campos@outlook.com>
2023-03-22 15:14:56 -0300

    feat: update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>CoinSynch</title>
-        <link rel="shortcut icon" href="/img/union.svg" />
+        <title>ViaCep</title>
+        <link rel="shortcut icon" href="/img/sebrae.svg" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0609B" />
         <meta
           name="description"
-          content="CoinSynch is a frontend challege with TypeScript, React, NextJS and Styled Components"
+          content="ViaCep is a frontend challege with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyles />
